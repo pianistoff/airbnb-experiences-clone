83606d5282552fbbf4cf0640ecf44ec65c0ab3f1
pianistoff <severyn.h@gmail.com>
2022-05-31 15:45:05 +0300

    Mark up and style Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,11 @@
+import photo from "../img/photo-grid.png"
+
 export default function Hero() {
     return (
-        <p>hello</p>
+        <div className="hero">
+            <img src={photo} alt="A photo grid showing people doing different activities." className="photo-grid" />
+            <h1 className="title">Online Experiences</h1>
+            <p className="para">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
     )
 }
