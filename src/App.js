bdfb372f1483f418ec1c471f8bd0fe59f6e4f210
pianistoff <severyn.h@gmail.com>
2022-06-06 08:34:42 +0300

    Make scrollbar appear only when there is lack of space

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import data from "./data"
+import data from "./data";
 
 export default function App() {
-    const cards = data.map(c => 
-        <Card 
-        key={c.id}
-        badge={c.badge}
-        img={c.coverImg}
-        rating={c.stats.rating}
-        reviewCount={c.stats.reviewCount}
-        country={c.location}
-        title={c.title}
-        price={c.price}
-        openSpots={c.openSpots}
-        />
-        )
+    const cards = data.map(card => (<Card key={card.id} {...card} />));
     return (
         <div className="app">
             <Navbar />
             <Hero />
-            <div className="cards-list">
-            {cards}
-            </div>
+            <div className="cards-list">{cards}</div>
         </div>
-    );
-}
+    )
+    }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,28 @@
 import star from "../img/star.svg";
 import ellipse from "../img/ellipse.svg";
 
-export default function Card({openSpots, img, rating, reviewCount, country, title, price}) {
+export default function Card({title, price, coverImg, stats, location, openSpots}) {
     let badgeText
     if (openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (country === "Online") {
+    } else if (location === "Online") {
         badgeText = "ONLINE"
     }
     return (
         <div className="card">
-            <div className="photo-container" style={{backgroundImage: `url(./img/${img})`}}>
+            <div className="photo-container" style={{backgroundImage: `url(./img/${coverImg})`}}>
                 {badgeText && <p className="badge">{badgeText}</p>}
             </div>
             <div className="info">
                 <img src={star} alt="rating star" className="star" />
-                <p className="rating">{rating}</p>
-                <p className="review-count">({reviewCount})</p>
+                <p className="rating">{stats.rating}</p>
+                <p className="review-count">({stats.reviewCount})</p>
                 <img
                     src={ellipse}
                     alt="dot in the center"
                     className="ellipse"
                 />
-                <p className="country">{country}</p>
+                <p className="country">{location}</p>
                 <p className="description">{title}</p>
                 <p className="price">
                     From ${price} <span className="per">/ person</span>
